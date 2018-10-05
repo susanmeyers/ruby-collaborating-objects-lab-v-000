1d1ce03d5b142a1ca48e0ac6bc05098742efae12
Susan Meyers <smmeyers24@gmail.com>
2018-10-05 00:03:53 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,24 @@
-class MP3_Importer
+class MP3Importer
   
+  
+  attr_accessor :path
+  
+  def initialize(test_music_path)
+ #binding.pry
+    @path = test_music_path 
+  end
+  
+  def files
+    Dir.entries(@path).select {|file| file.include?(".mp3")}
+  end
+  
+  def import
+    # binding.pry
+    files
+    music_importer = song.new_by_file_name("mp3")
+  
+  end  
 end
+
+#files returns an array of all mp3's 
+#create a new song by $FILENAME pass
